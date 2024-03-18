b9a9c4c671fd83f65032bb1466add6993432948b
yadimashar2013 <162845121+yadimashar2013@users.noreply.github.com>
2024-03-18 14:08:38 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+list_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in range(5):
+    print(f'Я езжу на автомабиле марки {list_[i]}')
+cars_count = 0
+for i in range(0, 10, 2):  # значение 10 взято произвольно.
+    cars_count += 10
+    print(cars_count)
